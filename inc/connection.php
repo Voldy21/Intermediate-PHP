cbba739b3a37572fc7bed9e21b72155867c3335b
Voldy21 <jonahlvngstn@gmail.com>
2020-04-05 00:57:52 +0000

    set PDO attribute attr_errmode

--- inc/connection.php
+++ inc/connection.php
@@ -1,9 +1,10 @@
 <?php
 try{
   $db = new PDO("sqlite:".__DIR__."/database.db");
-  var_dump($db);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e){
- echo "Unable to connect";
+  echo "Unable to connect  ";
+  echo $e->getMessage();
    exit;
 }
 
